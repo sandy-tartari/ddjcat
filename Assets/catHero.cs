f48fc90cdf7fbdcb6f526d7990196e75dbf619be
Sandy Tartari <andy17032001@gmail.com>
2021-03-21 19:12:26 -0300

    teste de commit

--- Assets/catHero.cs
+++ Assets/catHero.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class catHero : MonoBehaviour
 {
     // Start is called before the first frame update
-     public float velocity;
+    private Rigidbody2D body;
     private SpriteRenderer spriteRenderer;
+    private bool jump;
+    public float velocity;
+    public float jumpForce;
     // Start is called before the first frame update
     void Start()
-    {
+    { 
+        this.jump = false;
+        this.body = this.GetComponent<Rigidbody2D>();
         this.spriteRenderer = GetComponent<SpriteRenderer>();
         Debug.Log("O Maguin está na área!");
     }
@@ -17,21 +22,28 @@ public class catHero : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Para frente e para trás
         float inputKeyHorizontal = Input.GetAxis("Horizontal");
-        float inputKeyVertical = Input.GetAxis("Vertical");
-        float x = this.transform.position.x;
-        float y = this.transform.position.y;
-
+        // Para cima e para baixo
+        // float inputKeyVertical = Input.GetAxis("Vertical");
         if (inputKeyHorizontal > 0)
         {
-            this.transform.position = new Vector2(x + this.velocity, y);
-            this.spriteRenderer.flipX = false;
+            this.body.AddForce(new Vector2(this.velocity, 0));
+            this.spriteRenderer.flipX = true;
         }
         else if (inputKeyHorizontal < 0)
         {
-            this.transform.position = new Vector2(x - this.velocity, y);
-            this.spriteRenderer.flipX = true;
+            this.body.AddForce(new Vector2(-this.velocity, 0));
+            this.spriteRenderer.flipX = false;
+        }
+        if (Input.GetButton("Jump") && !this.jump)
+        {
+            this.jump = true;
+            this.body.AddForce(new Vector2(0, this.jumpForce)); 
+        }
+        else if (this.body.velocity.y == 0 && this.jump )
+        {
+            this.jump = false;
         }
     }
-
 }
